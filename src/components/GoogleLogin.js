5dcf6571398547cd59bf77110b6d38dedffd70a6
YugaIde <yuga.ide@arsaga.jp>
2023-05-16 13:31:58 +0900

    ログイン修正

--- src/components/GoogleLogin.js
+++ src/components/GoogleLogin.js
@@ -1,40 +1,39 @@
 import { useNavigate, Navigate } from 'react-router-dom';
 import { useState } from 'react';
-import { auth, provider } from '../firebase';
+import { auth, provider, signInWithPopup } from '../firebase';
 import { useAuthContext } from '../context/AuthContext';
 import logo from '../logo.svg';
 import './GoogleLogin.css';
 
 const Login = () => {
-    const {user} = useAuthContext();
-  const navigate = useNavigate();
-  const [error, setError] = useState('');
+    const { user } = useAuthContext();
+    const navigate = useNavigate();
+    const [error, setError] = useState('');
 
-  const handleLogin = async () => {
-    try {
-      await auth.signInWithPopup(provider);
-      navigate('/');
-    } catch (error) {
-      console.log(error);
-      setError(error.message);
+    const handleLogin = async () => {
+        try {
+            await signInWithPopup(auth, provider);
+            navigate('/');
+        } catch (error) {
+            console.log(error);
+            setError(error.message);
+        }
+    };
+
+    if (user) {
+        return <Navigate to="/" replace />;
     }
-  };
 
-  if (user) {
-    return <Navigate to="/" replace />;
-  }
-
-  return (
-    <div class="login">
-      <div>
-      <h1>front chat app</h1>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <button onClick={handleLogin}>Googleログイン</button>
-      </div>
-      <img src={logo} className="App-logo" alt="logo" />
-
-    </div>
-  );
+    return (
+        <div className="login">
+            <div>
+                <h1>front chat app</h1>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+                <button onClick={handleLogin}>Googleログイン</button>
+            </div>
+            <img src={logo} className="App-logo" alt="logo" />
+        </div>
+    );
 };
 
 export default Login;
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
@@ -13,10 +13,10 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export const auth = getAuth();
-export const provider = new GoogleAuthProvider();
-
-export { db, storage };
+export { auth, provider, signInWithPopup, db, storage };
